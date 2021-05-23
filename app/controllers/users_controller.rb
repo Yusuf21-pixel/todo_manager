4fd031d39f1c658fb22ba9f6563a6b338bd8d05e
yusuf <yusufdon001@gmail.com>
2021-05-23 21:16:56 +0530

    Create a users table, user model and users controller. Add a function in user.rb to render users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+# users_controller.rb
+class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    render plain: User.all.map { |user| user.to_pleasant_string }.join("\n")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # get "todos", to: "todos#index"
-  # post "todos/create", to: "todos#create"
-  # get "todos/:id", to: "todos#show"
   resources :todos
+  resources :users
 end
