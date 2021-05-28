8425cda38808121cfabbc14e7be0f4b73960cb25
yusuf <yusufdon001@gmail.com>
2021-05-29 00:25:53 +0530

    Check heroku

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,7 +3,7 @@ class TodosController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
+    # render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
   end
 
   def show
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,33 @@ class Todo < ActiveRecord::Base
     iscompleted = completed ? "[X]" : "[ ]"
     "#{id}. #{due_date.to_s(:long)} #{todo_text} #{iscompleted}"
   end
+
+  def self.overdue
+    where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
+  end
+
+  def self.mark_as_complete!(todo_id)
+    # find_by returns the object if a record is present with the given id otherwise It will return nil.
+    todo = find_by(id: todo_id)
+    if todo.nil?
+      puts "Invalid id #{todo_id} does not exist..."
+      exit
+    else
+      todo.completed = true
+      todo.save
+      todo
+    end
+  end
 end
