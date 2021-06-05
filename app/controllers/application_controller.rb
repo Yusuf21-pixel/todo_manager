7dc63240fff9446d629796a523d0b61914e5c4d1
yusuf <yusufdon001@gmail.com>
2021-06-05 16:52:45 +0530

    Add sign-out functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,10 @@ class ApplicationController < ActionController::Base
       nil
     end
   end
+
+  def destroy
+    session[:current_user_id] = nil
+    @current_user = nil
+    redirect_to "/"
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 # users_controller.rb
 class UsersController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  skip_before_action :ensure_user_logged_in
 
   def new
     render "new"
@@ -12,11 +12,5 @@ class UsersController < ApplicationController
                  email: params[:email],
                  password: params[:password])
     redirect_to "/"
-  end
-
-  def login
-    user = User.find_by(email: params[:email])
-    # If the record is not found or password doesn't match it will render false else it will render true.
-    render plain: ((!user.nil?) && (user.password == params[:password])) ? "True" : "False"
   end
 end
