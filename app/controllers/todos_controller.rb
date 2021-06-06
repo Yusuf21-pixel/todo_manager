8232ba08a2645c701fb199f03e763fb48bcbf103
yusuf <yusufdon001@gmail.com>
2021-06-07 03:02:24 +0530

    Validate todos before saving

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,20 +7,23 @@ class TodosController < ApplicationController
 
   def create
     todo_text = params[:todo_text]
-    due_date = DateTime.parse(params[:due_date])
-    new_todo = Todo.create!(
+    due_date = params[:due_date]
+    new_todo = Todo.new(
       todo_text: todo_text,
       due_date: due_date,
       completed: false,
       user_id: current_user.id,
     )
+    if !new_todo.save
+      flash[:error] = new_todo.errors.full_messages.join(", ")
+    end
     redirect_to todos_path
   end
 
   def update
     id = params[:id]
     completed = params[:completed]
-    todo = Todo.of_user(current_user)
+    todo = Todo.of_user(current_user).find(id)
     todo.completed = completed
     todo.save!
     redirect_to todos_path
@@ -28,7 +31,7 @@ class TodosController < ApplicationController
 
   def destroy
     id = params[:id]
-    todo = Todo.of_user(current_user)
+    todo = Todo.of_user(current_user).find(id)
     todo.destroy
     redirect_to todos_path
   end
